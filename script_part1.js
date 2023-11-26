c0d3d787abf9991e9274fbdb2c33ff89c7dea876
firdavsm1901 <firdavs.muhammadjonov19@gmail.com>
2023-11-26 20:54:00 +0600

    Assignment11_Part1

---
+++ script_part1.js
@@ -0,0 +1,8 @@
+var part1 = new Vue({
+    el: '#part1',
+    data: {
+        message: 'Hello, Vue!',
+
+        imageSource: 'https://austingil.com/wp-content/uploads/Vue-Blog-Cover.png'
+    }
+});
